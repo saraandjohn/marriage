d7e168a1e6dc3646d86f938c8aad1f1882dd9900
LucaBn <luca.bonotti@virgilio.it>
2022-06-01 17:25:35 +0200

    feat(One Page wedding static website): :sparkles: first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # marriage
+Static one-page website created to celebrate Sara and John wedding! 🎉
+
+## Dependencies:
+1. Bootstrap v5.1.3
+2. Swiper 8.1.6
---
+++ js/app.js
@@ -0,0 +1,46 @@
+// Back to top
+const backToTop = () => {
+  window.scroll({ top: 0, left: 0, behavior: "smooth" });
+};
+
+const backToTopElements = document.getElementsByClassName("back-to-top");
+for (let i = 0; i < backToTopElements.length; i++) {
+  backToTopElements[i].addEventListener("click", backToTop, false);
+}
+
+// Navbar Handler
+const navbarHandler = () => {
+  const navigationAnchorPointCollection = document.getElementsByClassName(
+    "navigation-anchor-point"
+  );
+  const navLinkCollection = document.getElementsByClassName("lbn-navbar-link");
+  let activeSectionIndex = 0;
+
+  for (let i = 0; i < navigationAnchorPointCollection.length; i++) {
+    navLinkCollection[i].classList.remove("active");
+    if (
+      navigationAnchorPointCollection[i].getBoundingClientRect().top <
+      window.innerHeight / 4
+    ) {
+      activeSectionIndex = i;
+    }
+  }
+
+  navLinkCollection[activeSectionIndex].classList.add("active");
+};
+
+// Scroll events handler
+const scollEventsHandler = () => {
+  navbarHandler();
+};
+
+// DOM Loaded events handler
+DOMLoadedEventsHandler = () => {
+  navbarHandler();
+};
+
+// Scroll event
+document.addEventListener("scroll", scollEventsHandler);
+
+// DOM Loaded event
+document.addEventListener("DOMContentLoaded", DOMLoadedEventsHandler, false);
